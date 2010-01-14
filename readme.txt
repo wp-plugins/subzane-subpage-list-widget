355546e0f4c14bfabc92670939aaa894ddba7eb2
subzane <subzane@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-14 14:50:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/subzane-subpage-list-widget/trunk@193892 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,16 +1,16 @@
-=== Plugin Name ===
+=== SubZane Sub Page List Widget ===
 Contributors: Andreas Norman
 Donate link: 
 Tags: pages, widget, subpages, list
 Requires at least: 2.5
-Tested up to: 2.8.3
-Stable tag: 1.2.1
+Tested up to: 2.9.1
+Stable tag: 1.2.2
 
-Lists all subpages from a selected parent page.
+Lists all subpages from a selected parent page with option do exclude specific pages. 
 
 == Description ==
 
-This plugin simply lists all subpages from a selected parent page in the sidebar. This widget can be placed many times in your sidebar.
+This plugin simply lists all subpages from a selected parent page (with option do exclude specific pages) in the sidebar. This widget can be placed many times in your sidebar.
 
 == Installation ==
 
@@ -20,7 +20,7 @@ This plugin simply lists all subpages from a selected parent page in the sidebar
 
 == Frequently Asked Questions ==
 
-For any questions or suggestions regarding the plugin please visit http://www.subzane.com
+For any questions or suggestions regarding the plugin please visit http://www.andreasnorman.se
 
 == Screenshots ==
 
--- subzane-subpagelist-widget.php
+++ subzane-subpagelist-widget.php
@@ -4,27 +4,9 @@ Plugin Name: SubZane Subpage List Widget
 Plugin URI: http://www.subzane.com/projects/subpage-list-widgetsubpage-list-widget/
 Description: Lists all subpages from a selected parent page.
 Author: Andreas Norman
-Version: 1.2.1
-Author URI: http://www.subzane.com
+Version: 1.2.2
+Author URI: http://www.andreasnorman.se
 */
-
-/*  Copyright 2009  Andreas Norman  (email : hello@subzane.com)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
 
 function SZSubPageListWidget($args, $widget_args = 1) {
 	extract( $args, EXTR_SKIP );
